8edba5576655091101e58fcae9ee274c700aa263
Jobsidney <jobsidney67@gmail.com>
2022-07-14 12:30:56 +0300

    Adding setStates

--- src/components/CountDown.js
+++ src/components/CountDown.js
@@ -4,14 +4,25 @@ export class CountDown extends React.Component{
 
     constructor(props){
         super(props)
+        this.state= {countDown: 100}
     }
+    componentDidMount(){ this.update()}
+
+    update(){
+        let currentCOunt=this.state.countDown=1;
+        setInterval(() => this.setState({countDown: currentCOunt-1}),1000)}
+        
+
+        
+        
+
 
 
     render(){
         return(
             <div>
                 <center>
-                    <h4 className="time-text">100</h4>
+                    <h4 className="time-text">{this.state.countDown}</h4>
                 </center>
             </div>
         )
