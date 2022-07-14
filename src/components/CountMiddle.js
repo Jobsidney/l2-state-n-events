3a723d7cb152505c1f549a12829c6f9e5899114a
Jobsidney <jobsidney67@gmail.com>
2022-07-14 12:38:06 +0300

    Param function

--- src/components/CountMiddle.js
+++ src/components/CountMiddle.js
@@ -6,12 +6,15 @@ function CountMiddle(){
     function addFifty(){
         setCount(50);
     }
+    function addValue(current){
+        setCount(current)
+    }
 
     return(
             <div>
                 <center>
                     <h4 className="btn-text">{count}</h4>
-                    <button onClick={addFifty} className="timeButton">CLICK ME</button>
+                    <button onClick={()=>addValue(2)} className="timeButton">CLICK ME</button>
                 </center>
             </div>
     )
